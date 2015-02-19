d835efa77e2f8d28907f753ad7f5604b71760d21
Daniel Bouman <d.bouman1@gmail.com>
2015-02-19 14:32:05 +0000

    implement periodic boundary condition

--- velocity_verlet.py
+++ velocity_verlet.py
@@ -6,14 +6,10 @@ from acceleration import acceleration
 def velocity_verlet(N, h, pos, a_0, L):
     v_half_h = np.add(pos,0.5*a_0*h)
     pos_h = np.add(pos,v_half_h*h)
-    if np.less(pos_h,0): # periodic boundary condition
-        pos_h = L - np.fabs(pos_h)
-
+    pos_h = np.where(pos_h<0,L-np.fabs(pos_h)) # periodic boundary condition
     a_h = acceleration(N, pos_h, L)
     v_h = np.add(v_half_h,0.5*a_h*h)
     a_0 = a_h
     pos = pos_h
-    if np.less(pos,0): # periodic boundary condition
-        pos = L - np.fabs(pos_h)
-
+    pos = np.where(pos<0,L-np.fabs(pos)) # periodic boundary condition
     return pos;

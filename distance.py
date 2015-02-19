9903d60ab3f6f9d501bf19ab87747404a0152398
Kenneth Goodenough <kenneth_saus@hotmail.com>
2015-02-19 11:06:48 +0100

    updated libraries in main

--- distance.py
+++ distance.py
@@ -1,14 +1,14 @@
 def distance(Position1,Position2):
-		distance_x = (Position2[1]-Position1[1])
-		if math.fabs(distance_x)>(L/2):
-			distance_x = distance_x - 2*math.fmod(distance_x,(L/2))
+		distance_x = (Position2[1]-Position1[1]) #Distance in the x-dimension between position 1 and 2. Does not yet take into account periodic boundary conditions
+		if np.fabs(distance_x)>(L/2):			#
+			distance_x = distance_x - 2*np.fmod(distance_x,(L/2))
 
 		distance_y = (Position2[2]-Position1[2])
-		if math.fabs(distance_y)>(L/2):
-			distance_y = distance_y - 2*math.fmod(distance_y,(L/2))
+		if np.fabs(distance_y)>(L/2):
+			distance_y = distance_y - 2*np.fmod(distance_y,(L/2))
 
 		distance_z = (Position2[3]-Position1[3])
-		if math.fabs(distance_z)>(L/2):
-			distance_z = distance_z - 2*math.fmod(distance_z,(L/2))
+		if np.fabs(distance_z)>(L/2):
+			distance_z = distance_z - 2*np.fmod(distance_z,(L/2))
 
-		distance = math.sqrt(math.pow(distance_x,2)+math.pow(distance_y,2)+math.pow(distance_z,2))
+		distance = np.sqrt(np.power(distance_x,2)+np.power(distance_y,2)+np.power(distance_z,2))
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 import pylab
 from mpl_toolkits.mplot3d import Axes3D
 
 
 L = 1 #Length of box
 M = 10 #M is the amount of unit cells per dimension
-N = 4*math.pow(M,3) #Number of particles. The factor 4 is for the fact that we have 4 particles per unit cell
+N = 4*np.power(M,3) #Number of particles. The factor 4 is for the fact that we have 4 particles per unit cell
 
 ##Initialization of particles
 #Homogeneous distribution

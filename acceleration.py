95579dedbde7294d4a40307c3a27d5e9e476bb25
Kenneth Goodenough <kenneth_saus@hotmail.com>
2015-02-19 13:18:27 +0100

    Correct version acceleration and velocity verlet

---
+++ acceleration.py
@@ -0,0 +1,12 @@
+import numpy as np
+def acceleration(pos,m):
+	for ii in xrange(0, N-1):
+		for iii in xrange(0, ii):
+			x_distance = distance(pos[ii,0],pos[iii,0])
+			y_distance = distance(pos[ii,1],pos[iii,1])
+			z_distance = distance(pos[ii,2],pos[iii,2])
+			F_x = 
+			F_y = 
+			F_z = 
+			a = float(F/m)
+    

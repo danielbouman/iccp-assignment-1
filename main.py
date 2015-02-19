8a1720fd13482dd6be815655370af1835efc9049
Daniel Bouman <d.bouman1@gmail.com>
2015-02-19 17:49:17 +0000

    function import

--- main.py
+++ main.py
@@ -6,28 +6,27 @@ import time
 from mpl_toolkits.mplot3d import Axes3D
 np.set_printoptions(threshold='nan')
 
+## Import functions
+from initpos_function import initpos
+from initvelocity import initvelocity
+from velocity_verlet import velocity_verlet
+
 ## Assign variables
-L = 100                                   # Box length
+L = 100                                 # Box length
 M = 1                                   # Unit cells per dimension
 N = 4*np.power(M,3)                     # Number of particles, 4 per unit cell
-h = 0.004 #timestep
+h = 0.004 								# timestep
 
 ## Init particle position, homogeneous distribution
-from initpos_function import initpos
 pos = initpos( L,N,M )
 
 ## Init velocity
-from initvelocity import initvelocity
 velocity = initvelocity( N )
 
 ## Init acceleration
 a_0 = np.zeros((N,3),dtype=float) #Initialize acceleration array
 
-
-
 ## Velocity verlet
-from velocity_verlet import velocity_verlet
-
 for t in xrange(0, 100):
 	pos = velocity_verlet( N, h, pos, velocity, a_0, L )
 	fig = pylab.figure()          	          # Define figure

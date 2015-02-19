c67f1566bc3085f012947ae5c2cddfe77ff65ef0
Daniel Bouman <d.bouman1@gmail.com>
2015-02-19 14:43:30 +0000

    fix periodic BC

--- velocity_verlet.py
+++ velocity_verlet.py
@@ -6,10 +6,10 @@ from acceleration import acceleration
 def velocity_verlet(N, h, pos, a_0, L):
     v_half_h = np.add(pos,0.5*a_0*h)
     pos_h = np.add(pos,v_half_h*h)
-    pos_h = np.where(pos_h<0,L-np.fabs(pos_h)) # periodic boundary condition
+    pos_h = np.where(pos_h<0,L-np.fabs(pos_h),pos_h) # periodic boundary condition
     a_h = acceleration(N, pos_h, L)
     v_h = np.add(v_half_h,0.5*a_h*h)
     a_0 = a_h
     pos = pos_h
-    pos = np.where(pos<0,L-np.fabs(pos)) # periodic boundary condition
+    pos = np.where(pos<0,L-np.fabs(pos),pos) # periodic boundary condition
     return pos;

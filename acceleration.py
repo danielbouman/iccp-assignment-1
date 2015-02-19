4c26751e9c6844c37e685148f16f76463c0721d5
Kenneth Goodenough <kenneth_saus@hotmail.com>
2015-02-19 13:26:41 +0100

    first draft acceleration

--- acceleration.py
+++ acceleration.py
@@ -5,8 +5,10 @@ def acceleration(pos,m):
 			x_distance = distance(pos[ii,0],pos[iii,0])
 			y_distance = distance(pos[ii,1],pos[iii,1])
 			z_distance = distance(pos[ii,2],pos[iii,2])
-			F_x = 
-			F_y = 
-			F_z = 
-			a = float(F/m)
-    
+			F_x = ((np.power(sigma,12))/(np.power(x_distance,13)))-((np.power(sigma,6))/(np.power(x_distance,7)))
+			F_y = ((np.power(sigma,12))/(np.power(y_distance,13)))-((np.power(sigma,6))/(np.power(y_distance,7)))
+			F_z = ((np.power(sigma,12))/(np.power(z_distance,13)))-((np.power(sigma,6))/(np.power(z_distance,7)))
+			a_x = F_x/m
+			a_y = F_y/m
+			a_z = F_z/m
+    return #moet hier niet nog een punt komma achter¿

9c275d4ed096e4631cb582a4e9ed4e8db58f5c9a
Daniel Bouman <d.bouman1@gmail.com>
2015-02-19 17:24:05 +0000

    fix plot

--- main.py
+++ main.py
@@ -24,46 +24,16 @@ velocity = initvelocity( N )
 a_0 = np.zeros((N,3),dtype=float) #Initialize acceleration array
 
 
-## Plot particles
-#fig = pylab.figure()                    # Define figure
-#ax = Axes3D(fig)                        # Define axis
-
-#ax.scatter(pos[:,0], pos[:,1], pos[:,2])# Plot positions
-#plt.
-#pylab.xlim([0,L])
-#pylab.ylim([0,L])
-#plt.ion()
-#plt.show()                              # Display plot
 
 
 ## Velocity verlet
 from velocity_verlet import velocity_verlet
 
-fig = pylab.figure()          	          # Define figure
-ax = Axes3D(fig)                        # Define axis
 for t in xrange(0, 100):
-	pos = velocity_verlet( N, h, pos, a_0, L )
-	#print pos_2	
-	#print pos
-	## Plot particles
-	#pos_3 = np.subtract(pos_2,pos)
-	#print pos_3
-	# pos = np.subtract(pos_2,pos)
-	#ax.clear()
-	#ax.scatter(pos[:,0], pos[:,1], pos[:,2])# Plot positions#
-	#pylab.xlim([0,L])
-	#pylab.ylim([0,L])
-	#plt.show()
-	#time.sleep(0.5)
-
-fig = pylab.figure()                    # Define figure
-ax = Axes3D(fig)                        # Define axis
-
-ax.scatter(pos[:,0], pos[:,1], pos[:,2])# Plot positions
-#plt.
-pylab.xlim([0,L])
-pylab.ylim([0,L])
-plt.ion()
-plt.show()                              # Display plot
-
-
+	pos_2 = velocity_verlet( N, h, pos, a_0, L )
+	fig = pylab.figure()          	          # Define figure
+	ax = Axes3D(fig)                        # Define axis
+	ax.scatter(pos_2[:,0], pos_2[:,1], pos_2[:,2])# Plot positions#
+	pylab.xlim([0,L])
+	pylab.ylim([0,L])
+	plt.show()     

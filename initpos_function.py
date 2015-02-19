38065fff3b25205675409c91f731419a99723267
Daniel Bouman <d.bouman1@gmail.com>
2015-02-19 10:57:40 +0000

    implement initpos function

--- initpos_function.py
+++ initpos_function.py
@@ -1,29 +1,29 @@
+import numpy as np
 def initpos(L, N, M):
 	# Given length of a box of D dimensions, distribute N particles homogeneous across
-	pos = 2 #np.zeros((N, 3),dtype=float) #Initialize position array
-	a = L/M #Lattice constant
+	pos = np.zeros((N, 3),dtype=float) #Initialize position array
+	a = float(L)/M #Lattice constant
 	j = 0 #counter that runs over all particles
-	for ii in xrange(0, M-1): # loop through unit cells in the x-direction
-		for iii in xrange(0, M-1): # loop through unit cells in the y-direction
-			for iiii in xrange(0, M-1): # loop through unit cells in the z-direction
-				pos[j,1] = a*ii 		#the first particle in each unit cell is added by adding the vector distance to the position of the unit cell
-				pos[j,2] = a*iii 		# y position of the first particle
-				pos[j,3] = a*iiii		# z position of the first particle
+	for ii in xrange(0, M): # loop through unit cells in the x-direction
+		for iii in xrange(0, M): # loop through unit cells in the y-direction
+			for iiii in xrange(0, M): # loop through unit cells in the z-direction
+				pos[j,0] = a*ii 		# the first particle in each unit cell is added by adding the vector distance to the position of the unit cell
+				pos[j,1] = a*iii 		# y position of the first particle
+				pos[j,2] = a*iiii		# z position of the first particle
 				j = j + 1 				# Move to next particle
-				pos[j,1] = a*ii 		#Now the same for the second particle at a different position
-				pos[j,2] = a*iii+0.5*a 	# y position of the second particle
-				pos[j,3] = a*iiii+0.5*a # z position of the second particle
+				pos[j,0] = a*ii 		# Now the same for the second particle at a different position
+				pos[j,1] = a*iii+0.5*a 	# y position of the second particle
+				pos[j,2] = a*iiii+0.5*a # z position of the second particle
 				j = j + 1 				# Move to next particle
-				pos[j,1] = a*ii+0.5*a 	# Now the same for the third particle at a different position
-				pos[j,2] = a*iii 		# y position of the third particle
-				pos[j,3] = a*iiii+0.5*a # z position of the third particle
+				pos[j,0] = a*ii+0.5*a 	# Now the same for the third particle at a different position
+				pos[j,1] = a*iii 		# y position of the third particle
+				pos[j,2] = a*iiii+0.5*a # z position of the third particle
 				j = j + 1 				# Move to next particle
-				pos[j,1] = a*ii+0.5*a 	# Now the same for the fourth particle at a different position
-				pos[j,2] = a*iii+0.5*a 	# y position of the fourth particle
-				pos[j,3] = a*iiii 		# z position of the fourth particle
+				pos[j,0] = a*ii+0.5*a 	# Now the same for the fourth particle at a different position
+				pos[j,1] = a*iii+0.5*a 	# y position of the fourth particle
+				pos[j,2] = a*iiii 		# z position of the fourth particle
 				j = j + 1 				# Move to next particle
 			#end for loop
 		#end for loop
 	#end for loop
-	
-return pos;
+	return pos;
--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 from mpl_toolkits.mplot3d import Axes3D
+np.set_printoptions(threshold='nan')
 
-
-L = 1 #Length of box
-M = 10 #M is the amount of unit cells per dimension
-N = 4*np.power(M,3) #Number of particles. The factor 4 is for the fact that we have 4 particles per unit cell
+L = 1 # box length
+M = 3 # unit cells per dimension
+N = 4*np.power(M,3) #Number of particles, 4 per unit cell
 
 ##Initialization of particles
 #Homogeneous distribution
 
 from initpos_function import initpos
 
-initpos( L,M,N );
+pos = initpos( L,N,M )
+print pos

64992186ad4ada034ba6a3ff2b458fc106f8e44b
Daniel Bouman <d.bouman1@gmail.com>
2015-02-19 12:23:45 +0000

    fix velocity verlet

--- velocity_verlet.py
+++ velocity_verlet.py
@@ -1,4 +1,8 @@
+# Import libraries
 import numpy as np
+# Import functions
+from acceleration import acceleration
+# Define velocity verlet function
 def velocity_verlet(L,D,N,h,pos,a_0):
     v_half_h = pos + 0.5*a_0*h
     pos_h = pos + v_half_h*h

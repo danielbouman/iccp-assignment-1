9ae70d8cece2dc59c254883a8370b775d634d401
Kenneth Goodenough <kenneth_saus@hotmail.com>
2015-02-19 11:36:35 +0100

    fix from where to where loop runs

--- distance.py
+++ distance.py
@@ -1,13 +1,13 @@
 def distance(Position1,Position2):
-		distance_x = (Position2[1]-Position1[1]) #Distance in the x-dimension between position 1 and 2. Does not yet take into account periodic boundary conditions
+		distance_x = (Position2[0]-Position1[0]) #Distance in the x-dimension between position 1 and 2. Does not yet take into account periodic boundary conditions
 		if np.fabs(distance_x)>(L/2):			#
 			distance_x = distance_x - 2*np.fmod(distance_x,(L/2))
 
-		distance_y = (Position2[2]-Position1[2])
+		distance_y = (Position2[1]-Position1[1])
 		if np.fabs(distance_y)>(L/2):
 			distance_y = distance_y - 2*np.fmod(distance_y,(L/2))
 
-		distance_z = (Position2[3]-Position1[3])
+		distance_z = (Position2[2]-Position1[2])
 		if np.fabs(distance_z)>(L/2):
 			distance_z = distance_z - 2*np.fmod(distance_z,(L/2))
 
--- initvelocity.py
+++ initvelocity.py
@@ -4,8 +4,8 @@ def initvelocity(L,D,N):
 	
 	# Assign random uniform velocity
 	for ii in xrange(0,N):
-			velocity[ii][1] = random.uniform(-5,5) #velocity is assigned uniformly between -5 and 5, this should be changed to boltzmann distribution!
+			velocity[ii][0] = random.uniform(-5,5) #velocity is assigned uniformly between -5 and 5, this should be changed to boltzmann distribution!
+			velocity[ii][1] = random.uniform(-5,5)
 			velocity[ii][2] = random.uniform(-5,5)
-			velocity[ii][3] = random.uniform(-5,5)
 			
 return;

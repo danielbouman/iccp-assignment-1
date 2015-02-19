65a715e9d6a698434b9c11d405bab45587494416
Kenneth Goodenough <kenneth_saus@hotmail.com>
2015-02-19 11:27:57 +0100

    remove spacing

--- initpos_function.py
+++ initpos_function.py
@@ -7,7 +7,7 @@ def initpos(L, N, M):
 		for iii in xrange(0, M-1): # loop through unit cells in the y-direction
 			for iiii in xrange(0, M-1): # loop through unit cells in the z-direction
 				pos[j,1] = a*ii 		#the first particle in each unit cell is added by adding the vector distance to the position of the unit cell
-				pos[j,2] = a*iii 	  	# y position of the first particle
+				pos[j,2] = a*iii 		# y position of the first particle
 				pos[j,3] = a*iiii		# z position of the first particle
 				j = j + 1 				# Move to next particle
 				pos[j,1] = a*ii 		#Now the same for the second particle at a different position

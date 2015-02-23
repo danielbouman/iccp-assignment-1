3f23a8f9ed7b523b691a88d9157809600df76dbc
Daniel Bouman <d.bouman1@gmail.com>
2015-02-23 18:53:54 +0000

    fix power

--- store_quantities.py
+++ store_quantities.py
@@ -1,6 +1,6 @@
 import numpy as np
 # Import functions
 def store_quantities(N,velocity):
-	kinetic_energy = 0.5*sum(sum(np.array(velocity)**2))
+	kinetic_energy = 0.5*(np.power(velocity,2))
 	total_velocity = sum(sum(np.array(velocity)))
 	return kinetic_energy,total_velocity;

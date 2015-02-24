798d69da86ea5b07645ef92082cce09e3b9a12a4
Kenneth Goodenough <kenneth_saus@hotmail.com>
2015-02-24 17:01:34 +0100

    specific heat

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from velocity_verlet import velocity_verlet
 from normalize_momentum import normalize_momentum
 from store_quantities import store_quantities
 from pressure import virial_pressure
+from specific_heat import specific_heat
 ## Assign variables
 L = 10                      # Box length
 M = 2                     	# Unit cells per dimension
@@ -35,6 +36,8 @@ pot_energy = np.zeros((time_dur),dtype=float)
 total_energy = np.zeros((time_dur),dtype=float)
 P = np.zeros((time_dur),dtype=float)
 T = np.zeros((time_dur),dtype=float)
+specific_heat_1 = np.zeros((time_dur),dtype=float)
+specific_heat_2 = np.zeros((time_dur),dtype=float)
 
 ## Init particle positions
 pos = initpos( L,N,M )
@@ -57,44 +60,16 @@ for t in xrange(0, time_dur):
     T[t] = (2/(3*kb*(N-1)))*kin_energy[t]
     total_energy[t] = np.add(kin_energy[t],pot_energy[t])
     #P[t] = virial_pressure(T,N,L,virial,r_c)
-    if np.mod(t,200) == 0:
+    # print total_energy[t]
+    if np.mod(t,200) == 0 and t<=1001:
     	velocity = normalize_momentum(N, velocity,T_d)
-    #total_energy[t] = np.add(kin_energy,pot_energy)
-    ## Dynamic plotting
-    # time[t] = t 
-    # pos_time[t] = pos[0,1]
-    # plt.xlim([0,L])
-    # plt.ylim([0,L])
-    # plt.show()
-    # ax = Axes3D(fig)                            # Define axis
-    # ax.scatter(pos[:,0], pos[:,1], pos[:,2])    # Plot positions#
-    ## Print
-    # print velocity[0,0]
-    # print kin_energy[t]
-    # print pos[1,2]
-    ## Write to output file
-    # out_vel = str(pot_energy[1,2]) + "\n"
-    # with open("output.dat", "a") as fh:
-    #     fh.write(out_vel)
-	if display_data == 'w':
-		out_energ = str(total_energy[t]) + "\n"
-		out_energ = out_energ.translate(None, '[]').replace(" ", "")
-		with open("total_energy.dat", "a") as f_energ:
-			f_energ.write(out_energ)
-		f_energ.close()
-		out_kin = str(kin_energy[t]) + "\n"
-		out_kin = out_kin.translate(None, '[]').replace(" ", "")
-		with open("kin_energy.dat", "a") as f_kin:
-			f_kin.write(out_kin)
-		f_kin.close()
-		out_pot = str(pot_energy[t]) + "\n"
-		out_pot = out_pot.translate(None, '[]').replace(" ", "")
-		with open("pot_energy.dat", "a") as f_pot:
-			f_pot.write(out_pot)
-		f_pot.close() # Close output file
+    if t>50:
+		specific_heat_1[t], specific_heat_2[t] = specific_heat(N,kb,T[t],total_energy[t-50:t],kin_energy[t-50:t])
+		#print "sp1"
+		#print specific_heat_1
+		#print "sp2"
+		#print specific_heat_2
 
-
-		
 if display_data == 'p':
 	out_energ = str(total_energy) + "\n"
 	out_energ = re.sub(' +',' ',out_energ)
@@ -128,3 +103,8 @@ if display_data == 'p':
 	plt.show()
 	plt.plot(time_step,T, 'b')
 	plt.show()
+	plt.plot(time_step,specific_heat_1, 'b')
+	plt.show()
+	plt.plot(time_step,specific_heat_2, 'b')
+	plt.show()
+	

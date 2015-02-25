pressure fix, averaging data

main.py
@@ -14,17 +14,25 @@ from store_quantities import store_quantities
 from pressure import virial_pressure
 from specific_heat import specific_heat
 ## Assign variables
-L = 10                      # Box length
-M = 2                     	# Unit cells per dimension
+#L = 4.969                      # Box length
+M = 3                       # Unit cells per dimension
 N = 4*np.power(M,3)         # Number of particles, 4 per unit cell
-h = 0.004 					# Timestep
-T_d = 100                   # desired temperature
-r_c = L*50 					# Cut off length in terms of L
-kb = 0.0083675				# Reduced Boltzmann constant
+h = 0.004                   # Timestep
+#T_d = 119.8                   # desired temperature
+r_c = 62.5                  # Cut off length in terms of L
+kb = 0.0083675              # Reduced Boltzmann constant
 
+
+rho = raw_input('Insert desired density (in units of 1/sigma^3') or 0.88
+T_d = raw_input('Insert desired temperature') or 119.8         # In units of timesteps
 display_data = raw_input('Write to file (w) or plot (p):') or 'w'
-time_dur = raw_input('Timesteps:') or 5000         # In units of timesteps
+time_dur = raw_input('Timesteps:') or 2400         # In units of timesteps
+rho = float(rho)
+L = np.power((N/rho),(1/3))
+T_d = float(T_d)
 time_dur = int(time_dur)
+
+
 
 time_step = np.zeros((time_dur),dtype=float)
 vel_time = np.zeros((time_dur),dtype=float)
@@ -35,9 +43,12 @@ total_velocity = np.zeros((time_dur),dtype=float)
 pot_energy = np.zeros((time_dur),dtype=float)
 total_energy = np.zeros((time_dur),dtype=float)
 P = np.zeros((time_dur),dtype=float)
+mean_P = np.zeros((time_dur),dtype=float)
 T = np.zeros((time_dur),dtype=float)
 specific_heat_1 = np.zeros((time_dur),dtype=float)
 specific_heat_2 = np.zeros((time_dur),dtype=float)
+diffusion_constant = np.zeros((time_dur),dtype=float)
+t_prog = 0
 
 ## Init particle positions
 pos = initpos( L,N,M )
@@ -50,61 +61,77 @@ a_0 = np.zeros((N,3),dtype=float) #Initialize acceleration array
 
 ## Plotting
 # fig = plt.figure()  # Define figure
-    
+	
 ## Time evolution
 for t in xrange(0, time_dur):
-    pos,velocity,a_0,potential,virial = velocity_verlet( N, h, pos, velocity, a_0, L )
-    time_step[t] = t*h
-    pot_energy[t] = 0.5*sum(potential)
-    kin_energy[t] = sum(sum(0.5*(np.power(velocity,2))))
-    T[t] = (2/(3*kb*(N-1)))*kin_energy[t]
-    total_energy[t] = np.add(kin_energy[t],pot_energy[t])
-    #P[t] = virial_pressure(T,N,L,virial,r_c)
-    # print total_energy[t]
-    if np.mod(t,200) == 0 and t<=1601:
-    	velocity = normalize_momentum(N, velocity,T_d)
-    if t>50:
+	pos,velocity,a_0,potential,virial = velocity_verlet( N, h, pos, velocity, a_0, L )
+	time_step[t] = t
+	pot_energy[t] = 0.5*sum(potential)
+	kin_energy[t] = sum(sum(0.5*(np.power(velocity,2))))
+	T[t] = (2/(3*kb*(N-1)))*kin_energy[t]
+	total_energy[t] = np.add(kin_energy[t],pot_energy[t])
+	#diffusion_constant[t] = np.divide(a_0,velocity)*kb*T[t]
+	P[t] = (virial_pressure(T[t],N,L,virial,r_c))/(kb*T[t]*rho)
+	# print total_energy[t]
+	if np.mod(t,20) == 0 and t<=801:
+		velocity = normalize_momentum(N, velocity,T_d)
+	if t>50:
 		specific_heat_1[t], specific_heat_2[t] = specific_heat(N,kb,T[t],total_energy[t-50:t],kin_energy[t-50:t])
+		mean_P[t] = np.mean(P[t-50:t])
 		#print "sp1"
 		#print specific_heat_1
 		#print "sp2"
 		#print specific_heat_2
+	# Print progress
+	if np.mod(t,time_dur/10) == 0:
+		print str(10-t_prog)
+		t_prog = t_prog+1
+
+out_energ = str(total_energy) + "\n"
+out_energ = re.sub(' +',' ',out_energ)
+out_energ = out_energ.translate(None, '[]').replace(" ", "\n")
+with open("total_energy.dat", "w") as f_energ:
+	f_energ.write(out_energ)
+f_energ.close()
+out_kin = str(kin_energy) + "\n"
+out_kin = re.sub(' +',' ',out_kin)
+out_kin = out_kin.translate(None, '[]').replace(" ", "\n")
+with open("kin_energy.dat", "w") as f_kin:
+	f_kin.write(out_kin)
+f_kin.close()
+out_pot = str(pot_energy) + "\n"
+out_kin = re.sub(' +',' ',out_kin)
+out_pot = out_pot.translate(None, '[]').replace(" ", "\n")
+with open("pot_energy.dat", "w") as f_pot:
+	f_pot.write(out_pot)
+f_pot.close()
+instant_temp = str(T) + "\n"
+instant_temp = re.sub(' +',' ',instant_temp)
+instant_temp = instant_temp.translate(None, '[]').replace(" ", "\n")
+with open("instant_temp.dat", "w") as f_temp:
+	f_temp.write(instant_temp)
+f_temp.close()
+pressure = str(P) + "\n"
+pressure = re.sub(' +',' ',pressure)
+pressure = pressure.translate(None, '[]').replace(" ", "\n")
+with open("pressure.dat", "w") as f_pres:
+	f_pres.write(pressure)
+f_pres.close()
 
 if display_data == 'p':
-	out_energ = str(total_energy) + "\n"
-	out_energ = re.sub(' +',' ',out_energ)
-	out_energ = out_energ.translate(None, '[]').replace(" ", "\n")
-	with open("total_energy.dat", "w") as f_energ:
-		f_energ.write(out_energ)
-	f_energ.close()
-	out_kin = str(kin_energy) + "\n"
-	out_kin = re.sub(' +',' ',out_kin)
-	out_kin = out_kin.translate(None, '[]').replace(" ", "\n")
-	with open("kin_energy.dat", "w") as f_kin:
-		f_kin.write(out_kin)
-	f_kin.close()
-	out_pot = str(pot_energy) + "\n"
-	out_kin = re.sub(' +',' ',out_kin)
-	out_pot = out_pot.translate(None, '[]').replace(" ", "\n")
-	with open("pot_energy.dat", "w") as f_pot:
-		f_pot.write(out_pot)
-	f_pot.close()
-	instant_temp = str(T) + "\n"
-	instant_temp = re.sub(' +',' ',instant_temp)
-	instant_temp = instant_temp.translate(None, '[]').replace(" ", "\n")
-	with open("instant_temp.dat", "w") as f_pot:
-		f_pot.write(instant_temp)
-	f_pot.close()
+	# plt.show()
+	# plt.plot(time_step,T, 'b')
+	# plt.show()
+	# plt.plot(time_step,specific_heat_1, 'b')
+	# plt.show()
+	# plt.plot(time_step,specific_heat_2, 'b')
+	# plt.show()
+	# plt.plot(time_step,diffusion_constant, 'g')
+	# plt.show()
 	# plt.plot(time_step,kin_energy, 'r', time_step,pot_energy, 'b',time_step,total_energy,'g')
 	# plt.show()
+	plt.plot(time_step,P,'k',time_step,mean_P,'b')
+	plt.show()
 	# plt.plot(time_step,pot_energy, 'b')
 	# plt.show()
 	# plt.plot(time_step,total_energy, 'b')
-	plt.show()
-	plt.plot(time_step,T, 'b')
-	plt.show()
-	plt.plot(time_step,specific_heat_1, 'b')
-	plt.show()
-	plt.plot(time_step,specific_heat_2, 'b')
-	plt.show()
-	

pressure.py
@@ -2,8 +2,9 @@
 import numpy as np
 ## Define function
 def virial_pressure(T,N,L,virial,r_c):
-    pressure = np.zeros((N,3),dtype=float)
-    kb = 1.1
-    V = np.power(L,3)
-    pressure = float((N/V)*kb*T - 1/(3*V)*virial-(2*np.pi*np.power(N,2))/(3*np.power(V,2))*(((48/11)*np.power(r_c,-11))-((24/5)*np.power(r_c,-5))))
-    return pressure;
+	pressure = np.zeros((N,3),dtype=float)
+	kb = 0.0083675
+	V = np.power(L,3)
+	pressure = (float(N)/V)*kb*T + 1/(3*float(V))*virial-(2*np.pi*np.power(N,2))\
+		/(3*np.power(V,2))*(((48/9)*np.power(r_c,-9))-((24/3)*np.power(r_c,-3)))
+	return pressure;
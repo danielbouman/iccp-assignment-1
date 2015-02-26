4d3e21f8e2bb381bd3d0a9a9a51fcf682892c3c9
Daniel Bouman <d.bouman1@gmail.com>
2015-02-26 11:18:20 +0000

    remove commented code

--- velocity_verlet.py
+++ velocity_verlet.py
@@ -5,18 +5,12 @@ from acceleration import acceleration
 # Define velocity verlet function
 def velocity_verlet(N, h, pos, v_0, a_0, L,cutoff, hist_bins):
     v_half_h = np.add(v_0,0.5*a_0*h)
-    # print v_half_h[1,2]
     pos_h = np.add(pos,v_half_h*h)
-    # print pos_h[1,2]
     # Impose periodic boundary condition
     pos_h = np.where(pos_h<0,L+pos_h,pos_h)
-    pos_h = np.where(pos_h>L, pos_h-L, pos_h) 
-    # print pos_h[1,2]
+    pos_h = np.where(pos_h>L, pos_h-L, pos_h)
     a_h,potential,virial,dist_hist = acceleration(N, pos_h, L, cutoff, hist_bins)
-    #print a_h[1,2]
-    # print a_h[1,2]
     v_h = np.add(v_half_h,(0.5*a_h*h))
-    # print v_h[1,2]
     a_0 = a_h
     pos = pos_h
     v_0 = v_h

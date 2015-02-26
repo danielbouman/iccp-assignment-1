caa564af934d0b8b42924aa9f71ff0ffb211a768
Daniel Bouman <d.bouman1@gmail.com>
2015-02-26 14:32:26 +0000

    remove unused functions

--- store_quantities.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-# Import functions
-def store_quantities(N,velocity):
-	kinetic_energy = sum(sum(0.5*(np.power(velocity,2))))
-	# print kinetic_energy
-	total_velocity = sum(sum(np.array(velocity)))
-	return (total_velocity, kinetic_energy)
